bfd277b25712e3fe46438a645ee4f55ae5cf768b
mypy <mypywelcome@gmail.com>
2023-10-27 02:40:26 +0400

    service_change

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,16 @@ import java.time.LocalDate;
 
 public class Main{
     public static void main(String[] args) {
-        String filePath = "src/three.txt";
-
+        
         FamilyThree three = new FamilyThree();
         Service service = new Service();
 
-        three.add(new Human("gor", Gender.Male, LocalDate.of(1990, 6, 2), null, null));
-        three.add(new Human("grigor", Gender.Male, LocalDate.of(2010, 4, 5), null, null));
+       three.add(new Human("gor", Gender.Male, LocalDate.of(1990, 6, 2), null, null));
+       three.add(new Human("grigor", Gender.Male, LocalDate.of(2010, 4, 5), null, null));
 
-        service.save(three, filePath);
-        service.readFile(filePath);
-        System.out.println(three.getInfo());
+       service.save(three);
+       // service.readFile();
+       System.out.println(three.getInfo());
         
     }
 
--- src/Service.java
+++ src/Service.java
@@ -1,21 +1,26 @@
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Service {
+    private String filePath;
     private Writable writable;
     private FamilyThree three;
-
+    
     public Service() {
+        this.filePath = "src/three.txt";
         this.writable = new FileHandler();     
+        this.three = new FamilyThree();
     }
 
-    public void save(Serializable serializable, String filePath) {
-        writable.saveToFile(three, filePath);
+    public void save(Serializable serializable) {
+        writable.saveToFile(serializable, filePath);
     }
 
-    public Object readFile(String filePath) {
-       return (Human) writable.readFromFile(filePath);
+    public Object readFile() {
+       return writable.readFromFile(filePath);
     }
 
 }
